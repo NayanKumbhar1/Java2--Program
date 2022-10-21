294edad6fa781cdc0194c37224a49db049db874e
NayanKumbhar1 <nayankumbhar1515@gmail.com>
2022-10-21 09:30:01 +0530

    [ADDED]:Nayan java code for Input From Users

---
+++ Sample.java
@@ -0,0 +1,14 @@
+class Sample
+{
+	public static void main(String args[])
+	{
+		int a,b,c=0;
+		System.out.println("InfoGalaxy Computers");
+		
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+
+		c=a+b;
+		System.out.println("Addition="+c);
+	}
+}
